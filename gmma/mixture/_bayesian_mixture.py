fix bug in covariance calculation in bayesian_mixutre add max covrance constraint

gmma/mixture/_bayesian_mixture.py
@@ -319,7 +319,7 @@ class BayesianGaussianMixture(BaseMixture):
                  degrees_of_freedom_prior=None, covariance_prior=None,
                  random_state=None, warm_start=False, verbose=0,
                  station_locs=None, phase_type=None, phase_weight=None, centers_init=None,
-                 dummy_comp=False, dummy_prob=0.01, loss_type="l1", bounds=None, max_covar=10,
+                 dummy_comp=False, dummy_prob=0.01, loss_type="l1", bounds=None, max_covar=None,
                  verbose_interval=10):
         super().__init__(
             n_components=n_components, tol=tol, reg_covar=reg_covar,
@@ -516,7 +516,7 @@ class BayesianGaussianMixture(BaseMixture):
         nk, xk, sk, centers = _estimate_gaussian_parameters(
             X, resp, self.reg_covar, self.covariance_type,
             self.station_locs, self.phase_type, loss_type=self.loss_type, 
-            centers_prev=None, bounds=self.bounds, max_covar=self.max_covar)
+            centers_prev=None, bounds=self.bounds)
 
         self._estimate_weights(nk)
         self._estimate_means(nk, xk)
@@ -578,7 +578,7 @@ class BayesianGaussianMixture(BaseMixture):
          }[self.covariance_type](nk, xk, sk)
 
         self.precisions_cholesky_ = _compute_precision_cholesky(
-            self.covariances_, self.covariance_type)
+            self.covariances_, self.covariance_type, self.max_covar)
 
     def _estimate_wishart_full(self, nk, xk, sk):
         """Estimate the full Wishart distribution parameters.
@@ -593,7 +593,7 @@ class BayesianGaussianMixture(BaseMixture):
 
         sk : array-like, shape (n_components, n_features, n_features)
         """
-        _, _, n_features = xk.shape
+        _, n_samples, n_features = xk.shape
 
         # Warning : in some Bishop book, there is a typo on the formula 10.63
         # `degrees_of_freedom_k = degrees_of_freedom_0 + Nk` is
@@ -607,7 +607,7 @@ class BayesianGaussianMixture(BaseMixture):
             self.covariances_[k] = (self.covariance_prior_ + nk[k] * sk[k] +
                                     nk[k] * self.mean_precision_prior_ /
                                     # self.mean_precision_[k] * np.outer(diff, diff))
-                                    self.mean_precision_[k] * np.dot(diff.T, diff))
+                                    self.mean_precision_[k] * np.dot(diff.T, diff)/n_samples)
 
         # Contrary to the original bishop book, we normalize the covariances
         self.covariances_ /= (
@@ -626,7 +626,7 @@ class BayesianGaussianMixture(BaseMixture):
 
         sk : array-like, shape (n_features, n_features)
         """
-        _, n_features = xk.shape
+        _, _, n_features = xk.shape
 
         # Warning : in some Bishop book, there is a typo on the formula 10.63
         # `degrees_of_freedom_k = degrees_of_freedom_0 + Nk`
@@ -656,7 +656,7 @@ class BayesianGaussianMixture(BaseMixture):
 
         sk : array-like, shape (n_components, n_features)
         """
-        _, n_features = xk.shape
+        _, _, n_features = xk.shape
 
         # Warning : in some Bishop book, there is a typo on the formula 10.63
         # `degrees_of_freedom_k = degrees_of_freedom_0 + Nk`
@@ -685,7 +685,7 @@ class BayesianGaussianMixture(BaseMixture):
 
         sk : array-like, shape (n_components,)
         """
-        _, n_features = xk.shape
+        _, _, n_features = xk.shape
 
         # Warning : in some Bishop book, there is a typo on the formula 10.63
         # `degrees_of_freedom_k = degrees_of_freedom_0 + Nk`
@@ -717,7 +717,7 @@ class BayesianGaussianMixture(BaseMixture):
         nk, xk, sk, self.centers_ = _estimate_gaussian_parameters(
             X, np.exp(log_resp), self.reg_covar, self.covariance_type,
             self.station_locs, self.phase_type, loss_type=self.loss_type, 
-            centers_prev=self.centers_, bounds=self.bounds, max_covar=self.max_covar)
+            centers_prev=self.centers_, bounds=self.bounds)
         self._estimate_weights(nk)
         self._estimate_means(nk, xk)
         self._estimate_precisions(nk, xk, sk)

gmma/mixture/_gaussian_mixture.py
@@ -274,7 +274,7 @@ def calc_mag(data, center, station_locs, weight):
     ## Atkinson, G. M. (2015). Ground-Motion Prediction Equation...
     # c0, c1, c2, c3, c4 = (-4.151, 1.762, -0.09509, -1.669, -0.0006)
     # mag_ = (data - c0 - c3*np.log10(dist))/c1
-    mag = np.sum(mag_ * weight) / (np.sum(weight)+10*np.finfo(weight.dtype).eps)
+    mag = np.sum(mag_ * weight) / (np.sum(weight)+1e-6)
     return mag
 
 def calc_amp(mag, center, station_locs):
@@ -302,7 +302,7 @@ def diff_and_grad(vars, data, station_locs, phase_type, vel={"p":6.0, "s":6.0/1.
     dist = np.sqrt(np.sum((station_locs - vars[:,:-1])**2, axis=1, keepdims=True))
     y = dist/v - (data - vars[:,-1:])
     J = np.zeros([data.shape[0], vars.shape[1]])
-    J[:, :-1] = (vars[:,:-1] - station_locs)/(dist + 1e-5)/v
+    J[:, :-1] = (vars[:,:-1] - station_locs)/(dist + 1e-6)/v
     J[:, -1] = 1
     return y, J
 
@@ -312,7 +312,7 @@ def newton_method(vars, data, station_locs, phase_type, weight, max_iter=20, con
         y, J = diff_and_grad(vars, data, station_locs, phase_type)
         JTJ = np.dot(J.T, weight * J)
         I = np.zeros_like(JTJ)
-        np.fill_diagonal(I, 1e-5)
+        np.fill_diagonal(I, 1e-3)
         vars -= np.dot(np.linalg.inv(JTJ + I) , np.dot(J.T, y * weight)).T
         if (np.sum(np.abs(vars - prev))) < convergence:
             return vars
@@ -325,7 +325,7 @@ def newton_method(vars, data, station_locs, phase_type, weight, max_iter=20, con
 #     vars = vars[np.newaxis, :]
 #     dist = np.sqrt(np.sum((station_locs - vars[:,:-1])**2, axis=1, keepdims=True))
 #     J = np.zeros([data.shape[0], vars.shape[1]])
-#     J[:, :-1] = (vars[:,:-1] - station_locs)/(dist + 1e-5)/v
+#     J[:, :-1] = (vars[:,:-1] - station_locs)/(dist + 1e-6)/v
 #     J[:, -1] = 1
 
 #     loss = np.sum(np.abs(dist/v - (data[:,-1:] - vars[:,-1:])) * weights)
@@ -340,7 +340,7 @@ def loss_and_grad(vars, data, station_locs, phase_type, weights, sigma=1, vel={"
     vars = vars[np.newaxis, :]
     dist = np.sqrt(np.sum((station_locs - vars[:,:-1])**2, axis=1, keepdims=True))
     J = np.zeros([data.shape[0], vars.shape[1]])
-    J[:, :-1] = (vars[:,:-1] - station_locs)/(dist + 1e-5)/v
+    J[:, :-1] = (vars[:,:-1] - station_locs)/(dist + 1e-6)/v
     J[:, -1] = 1
     
     y = dist/v - (data[:,-1:] - vars[:,-1:])
@@ -365,7 +365,7 @@ def l1_bfgs(vars, data, station_locs, phase_type, weight, max_iter=5, convergenc
 
 
 def _estimate_gaussian_parameters(X, resp, reg_covar, covariance_type,  station_locs,  phase_type, 
-                                  loss_type="l2", centers_prev=None, bounds=None, max_covar=20):
+                                  loss_type="l2", centers_prev=None, bounds=None):
     """Estimate the Gaussian distribution parameters.
 
     Parameters
@@ -440,12 +440,10 @@ def _estimate_gaussian_parameters(X, resp, reg_covar, covariance_type,  station_
                    "spherical": _estimate_gaussian_covariances_spherical
                    }[covariance_type](resp, X, nk, means, reg_covar)
 
-    covariances = max_covar * np.tanh(covariances/max_covar)
-
     return nk, means, covariances, centers
 
 
-def _compute_precision_cholesky(covariances, covariance_type):
+def _compute_precision_cholesky(covariances, covariance_type, max_covar=None):
     """Compute the Cholesky decomposition of the precisions.
 
     Parameters
@@ -492,6 +490,11 @@ def _compute_precision_cholesky(covariances, covariance_type):
         if np.any(np.less_equal(covariances, 0.0)):
             raise ValueError(estimate_precision_error_message)
         precisions_chol = 1. / np.sqrt(covariances)
+    
+    if max_covar is not None:
+        non_zero = (np.abs(precisions_chol) != 0.0)
+        precisions_chol[non_zero] = 1.0/(max_covar * np.tanh(1.0/precisions_chol[non_zero]/max_covar))
+
     return precisions_chol
 
 
@@ -769,7 +772,7 @@ class GaussianMixture(BaseMixture):
                  weights_init=None, means_init=None, precisions_init=None, centers_init=None,
                  random_state=None, warm_start=False, 
                  station_locs=None, phase_type=None, phase_weight=None, 
-                 dummy_comp=False, dummy_prob=0.01, loss_type="l1", bounds=None, max_covar=20,
+                 dummy_comp=False, dummy_prob=0.01, loss_type="l1", bounds=None, max_covar=None,
                  verbose=0, verbose_interval=10):
         super().__init__(
             n_components=n_components, tol=tol, reg_covar=reg_covar,
@@ -866,7 +869,7 @@ class GaussianMixture(BaseMixture):
         weights, means, covariances, centers = _estimate_gaussian_parameters(
             X, resp, self.reg_covar, self.covariance_type, 
             self.station_locs, self.phase_type, loss_type=self.loss_type, 
-            centers_prev=None, bounds=self.bounds, cov_max=self.max_covar)
+            centers_prev=None, bounds=self.bounds)
         weights /= n_samples
 
         # self.weights_ = (weights if self.weights_init is None else self.weights_init)
@@ -879,7 +882,7 @@ class GaussianMixture(BaseMixture):
         if self.precisions_init is None:
             self.covariances_ = covariances
             self.precisions_cholesky_ = _compute_precision_cholesky(
-                covariances, self.covariance_type)
+                covariances, self.covariance_type, self.max_covar)
         elif self.covariance_type == 'full':
             self.precisions_cholesky_ = np.array(
                 [linalg.cholesky(prec_init, lower=True)
@@ -906,10 +909,10 @@ class GaussianMixture(BaseMixture):
             _estimate_gaussian_parameters(
                 X, np.exp(log_resp), self.reg_covar, self.covariance_type, 
                 self.station_locs, self.phase_type, loss_type=self.loss_type, 
-                centers_prev=self.centers_, bounds=self.bounds, cov_max=self.max_covar))
+                centers_prev=self.centers_, bounds=self.bounds))
         self.weights_ /= n_samples
         self.precisions_cholesky_ = _compute_precision_cholesky(
-            self.covariances_, self.covariance_type)
+            self.covariances_, self.covariance_type, self.max_covar)
 
     def _estimate_log_prob(self, X):
         prob =  _estimate_log_gaussian_prob(X, self.means_, self.precisions_cholesky_, self.covariance_type)

test/example_bayersian.py
@@ -60,7 +60,7 @@ phase_type = phase_type[idx]
 label = label[idx]
 
 ## add false positive picks
-phase_fp = 0.5
+phase_fp = 1.0
 n_noise = int(len(data) * phase_fp)
 locs_noise = np.zeros([n_noise, station_loc.shape[-1]]) #n_phase, n_dim(x, y, z)
 data_noise = np.zeros([n_noise, 2]) #n_phase
@@ -138,13 +138,13 @@ mean_precision_prior = 0.1/delta_t
 if not use_amplitude:
     covariance_prior = np.array([[1]])
 else:
-    covariance_prior = np.array([[1,0],[0,1]])
+    covariance_prior = np.array([[1,0],[0,1]]) 
 print(f"time range: {delta_t:.3f}, amplitude range: {delta_amp:.3f}, mean precision prior: {mean_precision_prior:.3f}")
 
 gmm = mixture.BayesianGaussianMixture(n_components=num_event, 
                                       station_locs=locs, 
                                       phase_type=phase_type,
-                                      # weight_concentration_prior = 0.1/num_event,
+                                      weight_concentration_prior = 10000/num_event,
                                       mean_precision_prior = mean_precision_prior,
                                       covariance_prior = covariance_prior,
                                       # covariance_type='full', 
@@ -153,7 +153,9 @@ gmm = mixture.BayesianGaussianMixture(n_components=num_event,
                                       centers_init=centers_init.copy(),
                                       # dummy_comp=False, 
                                       # dummy_prob=dummy_prob,
-                                      loss_type="l1"
+                                      loss_type="l1",
+                                    #   max_covar=30.0,
+                                    #   reg_covar=0.1,
                                       ).fit(data)
 
 # gmm = mixture.GaussianMixture(n_components=num_event,