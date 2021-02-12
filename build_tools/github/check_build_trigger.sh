054b54f9ac2cc161428f01ecd3d11534e9b23a87
weiqiang <wayne.weiqiang@gmail.com>
2021-02-12 10:34:55 -0800

    skip check_build_trigger

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -6,7 +6,8 @@ set -x
 COMMIT_MSG=$(git log --no-merges -1 --oneline)
 
 # The commit marker "[cd build]" will trigger the build when required
-if [[ "$GITHUB_EVENT_NAME" == schedule ||
-      "$COMMIT_MSG" =~ \[cd\ build\] ]]; then
-    echo "::set-output name=build::true"
-fi
+# if [[ "$GITHUB_EVENT_NAME" == schedule ||
+#       "$COMMIT_MSG" =~ \[cd\ build\] ]]; then
+#     echo "::set-output name=build::true"
+# fi
+echo "::set-output name=build::true"
