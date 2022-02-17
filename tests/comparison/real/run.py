5cbad5ea715c7e049f061a0a974145269efb60d2
weiqiang <wayne.weiqiang@gmail.com>
2022-02-16 19:07:07 -0800

    finish comparison

--- tests/comparison/real/run.py
+++ tests/comparison/real/run.py
@@ -15,9 +15,8 @@ import shutil
 
 start=datetime.datetime(2019, 1, 1, 0, 0)
 end=datetime.datetime(2019, 1, 2, 0, 0)
-
 #make pick inputfile
-# picks = pd.read_csv('synthetic_picks.csv',header = None, sep = ',', names=['timestamp', 'type','jk1','jk2','sta','jk3'])
+# picks = pd.read_csv('synthetic_picks.csv',header = None, sep = '\t', names=['timestamp', 'type','jk1','jk2','sta','jk3'])
 picks = pd.read_csv('../synthetic_picks.csv',sep = '\t')
 picks.rename(columns={"station_id": "sta"}, inplace=True)
 picks['date'] = picks['timestamp']
@@ -34,12 +33,13 @@ for i in range((end-start).days):
         pick = picks.iloc[ii]
         phase_type = pick['type']
         phase_arrival = UTCDateTime(pick['timestamp']) - (UTCDateTime(start) + i * 86400)
-        if phase_type == 'p':
-            output_name = dateid+"/"+pick['sta'].split('.')[0] + '.' + pick['sta'].split('.')[1] + ".P.txt"
-        else:
-            output_name = dateid+"/"+pick['sta'].split('.')[0] + '.' + pick['sta'].split('.')[1] + ".S.txt"
-        with open(output_name, 'a') as file:
-            file.write('{} 1.0 0\n'.format(phase_arrival))
+        if phase_arrival< 1000:
+            if phase_type == 'p':
+                output_name = dateid+"/"+pick['sta'].split('.')[0] + '.' + pick['sta'].split('.')[1] + ".P.txt"
+            else:
+                output_name = dateid+"/"+pick['sta'].split('.')[0] + '.' + pick['sta'].split('.')[1] + ".S.txt"
+            with open(output_name, 'a') as file:
+                file.write('{} 1.0 0\n'.format(phase_arrival))
 
 #make station inputfile
 # stations = pd.read_csv('stations.csv', header = None, sep = ',', names = ['jk1', 'sta', 'stlo', 'stla', 'stev', 'jk2', 'jk3', 'jk4', 'jk5', 'jk6','jk7','jk8','jk9'])
@@ -54,11 +54,10 @@ with open('staton.dat', 'w') as f:
 start_time = time.time()
 for i in range((end-start).days):
     D = (start + datetime.timedelta(i)).strftime("%Y/%m/%d")+"/35.705"
-    R = "0.6/20/0.03/2/5/360/200/35.705/-117.504"
+    R = "0.6/20/0.03/2/5"
     G = "1/20/0.01/1"
     V = "6/3.428"
-    S = "3/3/8/2/0.5/0.1/1.8/0.35"
+    S = "0/0/10/0/1/0/2/0.2"
     command_line = "./REAL -D" + D + " -R" + R + " -S" + S + " -G" + G + " -V" + V  + " staton.dat ./" + (start + datetime.timedelta(i)).strftime("%Y%m%d") + " ./tt_db/ttdb.txt" 
-    print(command_line)
     os.system(command_line)
 print(f"--- {time.time() - start_time} seconds ---")
