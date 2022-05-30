e713c4defcf64543e646486b9cc4a634b7fd7a13
weiqiang <wayne.weiqiang@gmail.com>
2022-05-29 18:00:52 -0700

    add long_description

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     name='GMMA',
     version='1.0.3',
-    long_description="",
+    long_description="*GaMMA*: *Ga*ussian *M*ixture *M*odel *A*ssociation",
+    long_description_content_type="text/markdown",
     packages=['gamma'],
     install_requires=[
         'scikit-learn',
