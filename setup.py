64d65093b79d5056aabc780b46cdaa6a0524eb2d
Weiqiang <zhuwq0@gmail.com>
2022-08-07 21:02:01 -0700

    update format

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GMMA',
-    version='1.0.6',
+    version='1.0.7',
     long_description="*GaMMA*: *Ga*ussian *M*ixture *M*odel *A*ssociation",
     long_description_content_type="text/markdown",
     packages=['gamma'],
