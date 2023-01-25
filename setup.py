187b5acfc864a4116572b5cebe532bee097759f4
Weiqiang <75299929+zhuwq0@users.noreply.github.com>
2023-01-24 16:10:38 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name="GMMA",
-    version="1.0.11",
+    version="1.0.12",
     long_description="*GaMMA*: *Ga*ussian *M*ixture *M*odel *A*ssociation",
     long_description_content_type="text/markdown",
     packages=["gamma"],
     install_requires=[
         "scikit-learn",
+        "pyproj",
     ],
 )
