5ae520475003dc8d10d306407a057bdf6ffd7dd8
ziyi xi <ziyixi@mail.ustc.edu.cn>
2023-03-15 19:46:13 +0000

    remove the use of swift

--- gamma/utils.py
+++ gamma/utils.py
@@ -32,26 +32,10 @@ def trace_unhandled_exceptions(func):
 
 
 def convert_picks_csv(picks, stations, config):
-    import_swifter = True
-    try:
-        import swifter
-    except:
-        import_swifter = False
-        
     # t = picks["timestamp"].apply(lambda x: x.timestamp()).to_numpy()
     if type(picks["timestamp"].iloc[0]) is str:
-        if import_swifter:
-            picks["timestamp"] = picks["timestamp"].swifter.apply(lambda x: datetime.fromisoformat(x))
-        else:
-            picks["timestamp"] = picks["timestamp"].apply(lambda x: datetime.fromisoformat(x))
-    if import_swifter:
-        t = (
-        picks["timestamp"].swifter
-        .apply(lambda x: x.tz_convert("UTC").timestamp() if x.tzinfo is not None else x.tz_localize("UTC").timestamp())
-        .to_numpy()
-    )
-    else:
-        t = (
+        picks["timestamp"] = picks["timestamp"].apply(lambda x: datetime.fromisoformat(x))
+    t = (
         picks["timestamp"]
         .apply(lambda x: x.tz_convert("UTC").timestamp() if x.tzinfo is not None else x.tz_localize("UTC").timestamp())
         .to_numpy()
@@ -60,24 +44,15 @@ def convert_picks_csv(picks, stations, config):
     timestamp0 = np.min(t)
     t = t - timestamp0
     if config["use_amplitude"]:
-        if import_swifter:
-            a = picks["amp"].swifter.apply(lambda x: np.log10(x * 1e2)).to_numpy()
-        else:
-            a = picks["amp"].apply(lambda x: np.log10(x * 1e2)).to_numpy()  ##cm/s
+        a = picks["amp"].apply(lambda x: np.log10(x * 1e2)).to_numpy()  ##cm/s
         data = np.stack([t, a]).T
     else:
         data = t[:, np.newaxis]
     meta = stations.merge(picks["id"], how="right", on="id")
     locs = meta[config["dims"]].to_numpy()
-    if import_swifter:
-        phase_type = picks["type"].swifter.apply(lambda x: x.lower()).to_numpy()
-    else:
-        phase_type = picks["type"].apply(lambda x: x.lower()).to_numpy()
+    phase_type = picks["type"].apply(lambda x: x.lower()).to_numpy()
     phase_weight = picks["prob"].to_numpy()[:, np.newaxis]
-    if import_swifter:
-        pick_station_id = picks.swifter.apply(lambda x: x.id + "_" + x.type, axis=1).to_numpy()
-    else:
-        pick_station_id = picks.apply(lambda x: x.id + "_" + x.type, axis=1).to_numpy()
+    pick_station_id = picks.apply(lambda x: x.id + "_" + x.type, axis=1).to_numpy()
     nan_idx = meta.isnull().any(axis=1)
     return (
         data[~nan_idx],
