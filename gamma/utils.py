eff94e191024b891a9120f37df7955ac68940dbe
Ziyi Xi <ziyixi@mail.ustc.edu.cn>
2023-03-15 19:01:02 -0400

    use spawn, adjust chunk_size

--- gamma/utils.py
+++ gamma/utils.py
@@ -115,7 +115,8 @@ def association(picks, stations, config, event_idx0=0, method="BGMM", **kwargs):
             )
         )
 
-        with mp.Pool(config["ncpu"]) as p:
+        chunk_size = max(len(unique_labels)//(config["ncpu"]*20), 1)
+        with mp.get_context('spawn').Pool(config["ncpu"]) as p:
             results = p.starmap(
                 associate,
                 [
@@ -136,7 +137,8 @@ def association(picks, stations, config, event_idx0=0, method="BGMM", **kwargs):
                         lock,
                     ]
                     for k in unique_labels
-                ]
+                ],
+                chunksize=chunk_size,
             )
             # resuts is a list of tuples, each tuple contains two lists events and assignment
             # here we flatten the list of tuples into two lists
