036ee454f75c880a395ec07b72fe169fdb926d63
Weiqiang <zhuwq0@gmail.com>
2023-03-15 22:07:38 -0700

    reduce cpu numbers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="GMMA",
-    version="1.1.5",
+    version="1.1.6",
     long_description="*GaMMA*: *Ga*ussian *M*ixture *M*odel *A*ssociation",
     long_description_content_type="text/markdown",
     packages=["gamma"],
